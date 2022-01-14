197787d23516ca03b3913c4c8133200639111014
bM7tcHF88GBxDni <bM7tcHF88GBxDni@protonmail.com>
2022-01-14 16:01:17 +0000

    Amend datetime to display only date

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 function Time({ datetime }) {
+  function prettyDate() {
+    let date = datetime.split(":");
+    return date[0];
+  }
+
   return (
     <>
-      <p> {datetime}</p>
+      <p> {prettyDate()}</p>
     </>
   );
 }
