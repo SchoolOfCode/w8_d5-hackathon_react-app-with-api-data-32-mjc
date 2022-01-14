d596347e3d8071f64f95ef1891a33586d500ee27
casey <cassandra.d.mclellan@gmail.com>
2022-01-14 13:42:40 +0000

    fixed api fetch

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -17,13 +17,13 @@ function App() {
       const response = await fetch(`https://api.weatherbit.io/v2.0/current?key=ee51ab2f856b4b1394f20735c3a99968&city=${searchCity}`);
       const data = await response.json();
       const weatherData = {
-        datetime: data.datetime,
-        cityName: data.city_name,
-        countryCode: data.country_code,
-        temp: data.temp,
-        tempFeelsLike: data.app_temp,
-        //iconUrl: `https://www.weatherbit.io/static/img/icons/${data.weather.icon}.png`,
-        //description: data.weather.description
+        datetime: data.data[0].datetime,
+        cityName: data.data[0].city_name,
+        countryCode: data.data[0].country_code,
+        temp: data.data[0].temp,
+        tempFeelsLike: data.data[0].app_temp,
+        iconUrl: `https://www.weatherbit.io/static/img/icons/${data.data[0].weather.icon}.png`,
+        description: data.data[0].weather.description
       }
       setWeather(weatherData);
     }
