cb1e62ff188bcd4eed26fac9b6ad45f637e4c43d
ciisiq <cii_3@hotmail.com>
2022-01-14 14:42:21 +0000

    add style at the icon

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Icon({ iconUrl }) {
-  return <img src={iconUrl} alt=""></img>;
+  return (
+    <img
+      style={{
+        display: "block",
+        marginLeft: "auto",
+        marginRight: "auto",
+      }}
+      src={iconUrl}
+      alt=""
+    ></img>
+  );
 }
 
 export default Icon;
